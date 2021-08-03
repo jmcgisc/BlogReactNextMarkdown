c420c6420db3620c800bf11e6fcd555eaf3bd1a2
BuceApp <admin@buceapp.com>
2021-08-02 21:24:40 -0500

    antes de React

---
+++ pages/[slug].js
@@ -0,0 +1,27 @@
+import { getFileBySlug, getFiles } from "../lib/mdx";
+import { MDXRemote } from "next-mdx-remote"
+
+export default function Post ({ source, frontmatter}){
+    return <MDXRemote  {...source}/>
+}
+
+export async function getStaticProps ({ params }) {
+    const {source, frontmatter} = await getFileBySlug(params.slug);
+    return {
+        props: {source, frontmatter}
+    };
+}
+
+
+export async function getStaticPaths(){
+    const posts = await getFiles();
+    const paths = posts.map((post)=> ({
+        params: {
+            slug: post.replace(/\.mdx/, ""),
+        },
+    }));
+    return {
+        paths,
+        fallback: false,
+    };
+}
