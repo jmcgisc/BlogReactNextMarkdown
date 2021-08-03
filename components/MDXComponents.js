d4dfba707d338f3baa910f0127fc5da499fd1cdb
BuceApp <admin@buceapp.com>
2021-08-02 22:05:20 -0500

    con Podcast React

---
+++ components/MDXComponents.js
@@ -0,0 +1,6 @@
+import {Podcast} from "./Poadcast"
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    Podcast,
+};
--- components/Poadcast.js
+++ components/Poadcast.js
@@ -1,1 +1,13 @@
-export const Podcast({episode})
+export const Podcast = ({ episode }) =>
+{
+  return (
+    <iframe
+      src={`https://open.spotify.com/embed/episode/${episode}`}
+      width="100%"
+      height="232"
+      frameBorder="0"
+      allowtransparency="true"
+      allow="encrypted-media"
+    ></iframe>
+  )
+}
